035baf8ebd3ae78f679be28c7a92f40ad059d174
GargiChakraborty105 <107905817+D3V1L1805@users.noreply.github.com>
2025-01-05 12:08:10 +0530

    testing

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,32 @@
-(access_token, company_id, project_id)
-        # print(f'users : {users}')
+import psycopg2
+
+conn = psycopg2.connect("postgresql://palcode_ai:blaash@dpg-ctohkfd2ng1s73biuvr0-a.oregon-postgres.render.com/rfi_agent")
+cursor = conn.cursor()
+
+
+# Define the query with a parameterized placeholder
+query = """
+SELECT 
+    COUNT(*) AS current_workload
+FROM 
+    rfis
+WHERE 
+    status = 'open'
+    AND %s = ANY(assignees_id); 
+"""
+
+# Define the specific assignee ID you want to query
+assignee_id = 137269  # Replace with your desired ID
+
+# Execute the query with the parameter
+cursor.execute(query, (assignee_id,))
+
+conn.commit()
+
+project_instance = cursor.fetchall()
+
+print(project_instance)
+
+cursor.close()
+
+conn.close()
